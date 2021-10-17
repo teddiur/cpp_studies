f94fdc24904f4bb48f49798b6234f945b85ddfc4
Rodrigo Queiroz <rodrigo.yamagushi@stormgroup.com.br>
2021-10-17 13:21:34 -0300

    rename and code format

--- number_guess.cpp
+++ number_guess.cpp
@@ -1,19 +1,22 @@
 #include <cstdlib>
 #include <iostream>
-#include <ctime> 
+#include <ctime>
 using namespace std;
 
-string check_guess(int guess, int right_number){
-    if (guess > right_number){
+string check_guess(int guess, int right_number)
+{
+    if (guess > right_number)
+    {
         return "Muito alto! :c";
-    } 
-    else {
+    }
+    else
+    {
         return "Muito baixo! D:";
     }
-    
 }
 
-int get_guess(int current_try) {
+int get_guess(int current_try)
+{
     int guess;
     cout << "Tentativa " << current_try << endl;
     cout << "Escolha um número entre 1 e 20" << endl;
@@ -21,51 +24,59 @@ int get_guess(int current_try) {
     return guess;
 }
 
-int get_max_tries(){
+int get_max_tries()
+{
     char level;
     cout << "Escolha uma dificuldade de jogo" << endl;
     cout << "Fácil(F), Médio(M) ou Díficil(D)" << endl;
     cin >> level;
 
-    if (level == 'F'){
+    if (level == 'F')
+    {
         return 15;
-    } else if (level == 'M') {
+    }
+    else if (level == 'M')
+    {
         return 10;
-    } else {
+    }
+    else
+    {
         return 5;
     }
-
 }
 
-int main() {
+int main()
+{
     srand(time(NULL));
     const int our_number = rand() % 20 + 1;
     int user_guess = 0;
-    int current_try = 1; 
+    int current_try = 1;
     int max_tries;
 
     max_tries = get_max_tries();
 
     cout << "Você tem " << max_tries << " tentativas para descobrir o número" << endl;
 
-    for (current_try; current_try <= max_tries; current_try++) {
+    for (current_try; current_try <= max_tries; current_try++)
+    {
         user_guess = get_guess(current_try);
 
-        if(!user_guess) {
+        if (!user_guess)
+        {
             cin.clear();
             cin.ignore(10000, '\n');
             cout << "Digite um número, cabeçudo" << endl;
             continue;
         }
 
-        if (user_guess == our_number) {
+        if (user_guess == our_number)
+        {
             cout << "VOCÊ É GÊNIO!!!" << endl;
             cout << "Terminou em " << current_try << " tentativas" << endl;
             break;
         }
 
         cout << check_guess(user_guess, our_number) << endl;
-
     }
 
     return 0;
