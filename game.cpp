2b41fe76b80fc03f31da768abdca97fcc116a3d8
Rodrigo Queiroz <rodrigo.yamagushi@stormgroup.com.br>
2021-10-17 13:17:52 -0300

    add max_tries and refactor main

--- game.cpp
+++ game.cpp
@@ -5,22 +5,51 @@ using namespace std;
 
 string check_guess(int guess, int right_number){
     if (guess > right_number){
-        return "Muito alto";
+        return "Muito alto! :c";
     } 
     else {
-        return "Muito baixo";
+        return "Muito baixo! D:";
     }
     
 }
 
+int get_guess(int current_try) {
+    int guess;
+    cout << "Tentativa " << current_try << endl;
+    cout << "Escolha um número entre 1 e 20" << endl;
+    cin >> guess;
+    return guess;
+}
+
+int get_max_tries(){
+    char level;
+    cout << "Escolha uma dificuldade de jogo" << endl;
+    cout << "Fácil(F), Médio(M) ou Díficil(D)" << endl;
+    cin >> level;
+
+    if (level == 'F'){
+        return 15;
+    } else if (level == 'M') {
+        return 10;
+    } else {
+        return 5;
+    }
+
+}
+
 int main() {
     srand(time(NULL));
-    int our_number = rand() % 20 + 1;
+    const int our_number = rand() % 20 + 1;
     int user_guess = 0;
+    int current_try = 1; 
+    int max_tries;
 
-    while (true) {
-        cout << "Escolha um número entre 1 e 20" << endl;
-        cin >> user_guess;
+    max_tries = get_max_tries();
+
+    cout << "Você tem " << max_tries << " tentativas para descobrir o número" << endl;
+
+    for (current_try; current_try <= max_tries; current_try++) {
+        user_guess = get_guess(current_try);
 
         if(!user_guess) {
             cin.clear();
@@ -31,6 +60,7 @@ int main() {
 
         if (user_guess == our_number) {
             cout << "VOCÊ É GÊNIO!!!" << endl;
+            cout << "Terminou em " << current_try << " tentativas" << endl;
             break;
         }
 
