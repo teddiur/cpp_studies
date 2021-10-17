371768bdf39edb3f9768eeeb6603d967ee14aa2f
Rodrigo Queiroz <rodrigo.yamagushi@stormgroup.com.br>
2021-10-17 19:07:37 -0300

    init hangman game

---
+++ hangman.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+const string SECRET_WORD = "MELANCIA";
+map<char, bool> has_guessed;
+vector<char> wrong_guesses;
+
+bool char_in_string(char guess)
+{
+    for (char letter : SECRET_WORD)
+    {
+        if (guess == letter)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void greetings()
+{
+    cout << "*********************" << endl;
+    cout << "*** Jogo da Forca ***" << endl;
+    cout << "*********************" << endl
+         << endl;
+    cout << "Vamos jogar o jogo da forca!" << endl;
+}
+
+void print_wrong_guesses(vector<char> wrong_guesses)
+{
+    cout << "Chutes errados: ";
+    for (char letter : wrong_guesses)
+    {
+        cout << letter << " ";
+    }
+    cout << endl;
+}
+
+void print_hangman_letters(map<char, bool> has_guessed)
+{
+    for (char letter : SECRET_WORD)
+    {
+        if (has_guessed[letter])
+        {
+            cout << letter << " ";
+        }
+        else
+        {
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+char get_user_guess()
+{
+    char guess;
+    cout << "Chute uma letra:";
+    cin >> guess;
+    return toupper(guess);
+}
+
+bool got_all_letters()
+{
+    for (char letter : SECRET_WORD)
+    {
+        if (!has_guessed[letter])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool has_tries_left()
+{
+    return wrong_guesses.size() < 5;
+}
+
+int main()
+{
+    greetings();
+    while (has_tries_left() && !got_all_letters())
+    {
+
+        print_wrong_guesses(wrong_guesses);
+
+        print_hangman_letters(has_guessed);
+
+        char guess = get_user_guess();
+        has_guessed[guess] = true;
+
+        if (char_in_string(guess))
+        {
+            cout << "Você acertou, o seu chute está na palavra!" << endl;
+        }
+        else
+        {
+            cout << "Você errou, o seu chute não está na palavra!" << endl;
+            wrong_guesses.push_back(guess);
+        }
+        cout << endl;
+    }
+}
